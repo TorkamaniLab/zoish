878a4d7c329a9ecdc4e1a2ce7cbd0a69b8d3340c
drhosseinjavedani <h.javedani@gmail.com>
2022-06-26 00:36:05 -0700

    edit dockerfile

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 echo "hi from poetry"
-exec . /venv/bin/activate && poetry publish username=$username password=$password
+exec source/venv/bin/activate && poetry publish username=$username password=$password
