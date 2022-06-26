c9a27922e1197ea784766834186dd3a227d7d8ec
drhosseinjavedani <h.javedani@gmail.com>
2022-06-26 01:25:58 -0700

    edit dockerfile

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 echo "hi from poetry"
 
-echo username
 echo $username
+echo $password
 
-
-poetry publish --username=username --password=password
+poetry publish --username=$username --password=$password
 
