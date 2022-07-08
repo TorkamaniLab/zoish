24783aaf25f7de740b7a245eafbc24b61bb00cb5
drhosseinjavedani <h.javedani@gmail.com>
2022-07-08 14:18:12 -0700

    change jenkins file

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,6 +9,8 @@ echo $gitpassword
 git config --global user.email "h.javedani@gmail.com"
 git config --global user.name $gitusername
 git config --global user.password $gitpassword
+git remote set-url origin https://github.com/$gitusername/zoish.git 
+
 
 nox -s release -- minor
 
