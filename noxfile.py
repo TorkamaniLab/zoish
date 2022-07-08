cb0ce7cc304033a64dc74399131bdc8a9fcc1609
drhosseinjavedani <h.javedani@gmail.com>
2022-07-08 15:55:28 -0700

    change jenkins file

--- noxfile.py
+++ noxfile.py
@@ -1,31 +1,7 @@
 import nox
 import argparse
-import argparse
-import getpass
 
-class PasswordPromptAction(argparse.Action):
-    def __init__(self,
-             option_strings,
-             dest=None,
-             nargs=0,
-             default=None,
-             required=False,
-             type=None,
-             metavar=None,
-             help=None):
-        super(PasswordPromptAction, self).__init__(
-             option_strings=option_strings,
-             dest=dest,
-             nargs=nargs,
-             default=default,
-             required=required,
-             metavar=metavar,
-             type=type,
-             help=help)
 
-    def __call__(self, parser, args, values, option_string=None):
-        password = getpass.getpass()
-        setattr(args, self.dest, password)
 
 @nox.session(python=False)
 def tests(session):
@@ -79,7 +55,6 @@ def release(session):
     parser.add_argument(
         "gitpassword",
         type=str,
-        action=PasswordPromptAction,
         nargs=1,
         help="gitpassword for git"
     )
