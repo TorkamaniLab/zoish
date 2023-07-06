84bb378e414a069313e041471339e7cba988449a
Vigneshwaran <svigneshwaran1073@gmail.com>
2023-07-06 22:35:18 +0530

    Update shap_selectors.py

--- zoish/feature_selectors/shap_selectors.py
+++ zoish/feature_selectors/shap_selectors.py
@@ -14,7 +14,7 @@ from zoish.base_classes.best_estimator_getters import (
     BestEstimatorFindByTuneSearch,
 )
 
-logger.info("Single Shap Feature Selector has started !")
+logger.info("Shap Feature Selector has started !")
 
 
 class ShapPlotFeatures(PlotFeatures):
