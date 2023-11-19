ec3dd0ba56a9a1ff6070b6a072b3c9ebd7db073a
drhosseinjavedani <h.javedani@gmail.com>
2023-11-19 15:38:02 -0800

    revise tests

--- noxfile.py
+++ noxfile.py
@@ -8,7 +8,7 @@ test_files = [
     "tests/test_gpboost.py",
     "tests/test_ShapPlotFeatures.py",
     "tests/test_shap_feature_selector_with_n_feature_fasttreeshap.py",
-    "tests/test_shap_feature_selector_with_n_feature_shap.py",
+    #"tests/test_shap_feature_selector_with_n_feature_shap.py",
     "tests/test_shap_feature_selector_with_threshold_fasttreeshap.py",
     "tests/test_shap_feature_selector_with_threshold_shap.py",
     "tests/test_zoish.py"
